8cf750a0524ef3a19ec2d91e04fd0c26d883eb22
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 00:49:47 +0900

    Initial Scan

--- app/src/main/java/com/example/endline_v1/ScanBarCode.java
+++ app/src/main/java/com/example/endline_v1/ScanBarCode.java
@@ -1,15 +1,28 @@
 package com.example.endline_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanBarCode extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanpage);
+        new IntentIntegrator(this).initiateScan();
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "ISBN : " + result.getContents().toString(), Toast.LENGTH_SHORT).show();
     }
 }
