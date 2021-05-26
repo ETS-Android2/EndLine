5264de38299eb38f60c777a3cab6200a2edfbf15
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 00:44:47 +0900

    Basic Design Scan Page

--- app/src/main/java/com/example/endline_v1/ScanBarCode.java
+++ app/src/main/java/com/example/endline_v1/ScanBarCode.java
@@ -1,4 +1,15 @@
 package com.example.endline_v1;
 
-public class ScanBarCode {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanBarCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
