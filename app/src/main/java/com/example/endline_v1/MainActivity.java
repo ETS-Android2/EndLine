d0093b8c2f9def0c6eed209c34f658df0eeaa174
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 00:51:21 +0900

    Add ScanBarCode to Manifest

--- app/src/main/java/com/example/endline_v1/MainActivity.java
+++ app/src/main/java/com/example/endline_v1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.endline_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -30,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(this, ScanBarCode.class));
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
