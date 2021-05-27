50d748910102efa0d3338c15fd640deaaca8d560
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 16:11:00 +0900

    Check Permission Use Ted

--- app/src/main/java/com/example/endline_v1/LoadActivity.java
+++ app/src/main/java/com/example/endline_v1/LoadActivity.java
@@ -1,11 +1,18 @@
 package com.example.endline_v1;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class LoadActivity extends AppCompatActivity {
 
@@ -15,6 +22,30 @@ public class LoadActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(LoadActivity.this, "Granted Permission", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(LoadActivity.this, "Denied Permission", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("카메라 접근권한이 필요합니다.")
+                .setDeniedMessage("Denied")
+                .setPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CHANGE_NETWORK_STATE,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_NETWORK_STATE)
+                .check();
 
         handler = new Handler();
         handler.postDelayed(new Runnable() {
