bdd907683bef3ab975366a0f1d6f60e36082a9f2
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 12:14:17 +0900

    Create LoadActivity XML

--- app/src/main/java/com/example/endline_v1/LoadActivity.java
+++ app/src/main/java/com/example/endline_v1/LoadActivity.java
@@ -1,4 +1,16 @@
 package com.example.endline_v1;
 
-public class LoadActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_load);
+    }
 }
