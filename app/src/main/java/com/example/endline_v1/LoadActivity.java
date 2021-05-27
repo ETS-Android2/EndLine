57876d2fefbfeaae3bd23dc0235d814b3b8a2c69
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 14:53:00 +0900

    Implement Activity Change Animation

--- app/src/main/java/com/example/endline_v1/LoadActivity.java
+++ app/src/main/java/com/example/endline_v1/LoadActivity.java
@@ -1,6 +1,8 @@
 package com.example.endline_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
@@ -8,9 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoadActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_load);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 2000);
     }
 }
