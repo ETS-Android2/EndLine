510e76569d1313d015048bd3e199bac3ca936397
Enterprise09 <goldenstaytes@gmail.com>
2021-05-27 15:47:09 +0900

    Update OnCreate Instance

--- app/src/main/java/com/example/endline_v1/LoadActivity.java
+++ app/src/main/java/com/example/endline_v1/LoadActivity.java
@@ -3,7 +3,6 @@ package com.example.endline_v1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +12,8 @@ public class LoadActivity extends AppCompatActivity {
     Handler handler;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
 
         handler = new Handler();
@@ -22,7 +21,7 @@ public class LoadActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 finish();
             }
         }, 2000);
