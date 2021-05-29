f8e77b7f015b23c0f14565b93ea4eac9c40def42
Enterprise09 <goldenstaytes@gmail.com>
2021-05-29 22:16:06 +0900

    Add Login Menu

--- app/src/main/java/com/example/endline_v1/MainActivity.java
+++ app/src/main/java/com/example/endline_v1/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_beauty, R.id.nav_food, R.id.nav_health, R.id.nav_medical)
+                R.id.nav_beauty, R.id.nav_food, R.id.nav_health, R.id.nav_medical, R.id.nav_login)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
