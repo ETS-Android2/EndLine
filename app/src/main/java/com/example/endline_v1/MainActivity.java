5168ed8f37a2868a4180c557e980bf4e09d3ff8e
Enterprise09 <goldenstaytes@gmail.com>
2021-05-29 16:00:36 +0900

    Create resultLogin Method For Change View

--- app/src/main/java/com/example/endline_v1/MainActivity.java
+++ app/src/main/java/com/example/endline_v1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -12,10 +13,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -95,6 +101,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 resultLogin(account);   //Print Login Information
             }
         }
+    }
+
+    private void resultLogin(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {    //Check Login State
+                        if(task.isSuccessful()){    //Login Success
+                            Toast.makeText(MainActivity.this, "Success Login", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                            intent.putExtra("nickName", account.getDisplayName().toString());
+                            intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));     //Change to String
+                            startActivity(intent);      //Go to ResultActivity
+                        }else{  //Login Fail
+                            Toast.makeText(MainActivity.this, "Fail Login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     @Override
