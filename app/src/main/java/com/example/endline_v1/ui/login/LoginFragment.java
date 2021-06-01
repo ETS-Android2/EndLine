900f82c27d913a6498390694d67886b9359936c1
Enterprise09 <goldenstaytes@gmail.com>
2021-06-01 15:04:12 +0900

    Toast Msg by Logout Click Event

--- app/src/main/java/com/example/endline_v1/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/endline_v1/ui/login/LoginFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.endline_v1.MainActivity;
 import com.example.endline_v1.R;
 
 public class LoginFragment extends Fragment {
 
     private LoginViewModel loginViewModel;
+    private Button btn_logout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +34,16 @@ public class LoginFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        btn_logout = root.findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "로그아웃되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return root;
     }
 }
