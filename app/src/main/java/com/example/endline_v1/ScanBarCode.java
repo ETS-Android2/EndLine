80ad38a4f26bbaafdd352654f233547ba8e55ba9
Enterprise09 <goldenstaytes@gmail.com>
2021-10-20 12:46:07 +0900

    [+] put data to fire store with image url

--- app/src/main/java/com/example/endline_v1/ScanBarCode.java
+++ app/src/main/java/com/example/endline_v1/ScanBarCode.java
@@ -5,8 +5,10 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,21 +16,29 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -38,6 +48,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class ScanBarCode extends AppCompatActivity {
 
@@ -47,6 +58,10 @@ public class ScanBarCode extends AppCompatActivity {
     Spinner spinner;
     Map<String, Object> data = new HashMap<>();
     Calendar c = Calendar.getInstance();
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    Uri imgUri;
+    String randomKey;
 
     static final int REQ_SELECT_PHOTO = 111;
 
@@ -54,6 +69,10 @@ public class ScanBarCode extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanpage);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("등록");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         et_barcode = (EditText) findViewById(R.id.et_barcode);
         et_productName = (EditText) findViewById(R.id.et_productName);
@@ -112,7 +131,6 @@ public class ScanBarCode extends AppCompatActivity {
                             c.get(Calendar.MONTH),
                             c.get(Calendar.DAY_OF_MONTH)).show();
                 }
-
             }
         };
 
@@ -131,30 +149,55 @@ public class ScanBarCode extends AppCompatActivity {
         btn_insertScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (imgUri != null) {
+                    randomKey = UUID.randomUUID().toString();
+                    storage = FirebaseStorage.getInstance();
+                    storageRef = storage.getReference("images/" + randomKey);
+
+                    storageRef.putFile(imgUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                storageRef.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Uri> task) {
+                                        if(task.isSuccessful()){
+                                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                                            FirebaseAuth user = FirebaseAuth.getInstance();
+//                                            Toast.makeText(getApplicationContext(), user.getUid(), Toast.LENGTH_SHORT).show();
+                                            data.put("UID", user.getUid());
+                                            data.put("바코드 번호", et_barcode.getText().toString().substring(9));
+                                            data.put("등록 일자", getTime());
+                                            data.put("제품명", et_productName.getText().toString());
+                                            data.put("이미지", task.getResult().toString());
+                                            data.put("카테고리", et_category.getText().toString());
+                                            data.put("가격", et_price.getText().toString());
+                                            data.put("구매 일자", et_buyDay.getText().toString());
+                                            data.put("유통 기한", et_endline.getText().toString());
+                                            firestore.collection("mainData").add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                @Override
+                                                public void onSuccess(DocumentReference documentReference) {
+                                                    Log.d("Firestore 입력", "write data to firebase");
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w("firestore input", "write data fail");
+                                                }
+                                            });
+                                        }else{
+                                            Log.w("put Img", "fail");
+                                        }
+                                    }
+                                });
+                            }else{
+                                Log.w("put Img", "fail");
+                            }
+                        }
+                    });
+                }
                 finish();
                 Toast.makeText(getApplicationContext(), "입력 완료", Toast.LENGTH_SHORT).show();
-                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-                FirebaseAuth user = FirebaseAuth.getInstance();
-                Toast.makeText(getApplicationContext(), user.getUid(), Toast.LENGTH_SHORT).show();
-                data.put("UID", user.getUid());
-                data.put("바코드 번호", et_barcode.getText().toString().substring(9));
-                data.put("등록 일자", getTime());
-                data.put("제품명", et_productName.getText().toString());
-                data.put("카테고리", et_category.getText().toString());
-                data.put("가격", et_price.getText().toString());
-                data.put("구매 일자", et_buyDay.getText().toString());
-                data.put("유통 기한", et_endline.getText().toString());
-                firestore.collection("mainData").add(data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("Firestore 입력", "write data to firebase");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("firestore input", "write data fail");
-                    }
-                });
             }
         });
         
@@ -193,6 +236,7 @@ public class ScanBarCode extends AppCompatActivity {
                         inputStream.close();
 
                         ibtn_selectPhoto.setImageBitmap(img);
+                        imgUri = data.getData();
                     }catch (Exception e){
                         e.printStackTrace();
                     }
@@ -200,6 +244,12 @@ public class ScanBarCode extends AppCompatActivity {
                 break;
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onDestroy() {
