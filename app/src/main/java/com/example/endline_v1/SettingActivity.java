e6f1a931e05c382821291110a415782ca23fdcc7
Enterprise09 <goldenstaytes@gmail.com>
2021-11-05 01:04:57 +0900

    [+] RecyclerView to SettingActivity

--- app/src/main/java/com/example/endline_v1/SettingActivity.java
+++ app/src/main/java/com/example/endline_v1/SettingActivity.java
@@ -4,11 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.endline_v1.ui.health.RecyclerAdapter;
+
+import java.util.ArrayList;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private ArrayList<DataSet> list;
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
+    private Button btn_add, btn_clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +29,35 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("환경 설정");
+        actionBar.setTitle("알림");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
+        adapter = new RecyclerAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+        btn_add = (Button) findViewById(R.id.btn_itemadd);
+        btn_clear = (Button) findViewById(R.id.btn_itemdel);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataSet data = new DataSet("유통기한 임박 확인!!!");
+                list.add(data);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
     @Override
